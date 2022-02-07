d07dc2ed395e9ef2d4c6acaae5fc07e5dce84633
nathaniel-tavarez <98330315+nathaniel-tavarez@users.noreply.github.com>
2022-02-07 09:25:58 -0600

    Update PONG.py

--- PONG.py
+++ PONG.py
@@ -1,4 +1,8 @@
 import turtle as trtl
+wn = trtl.Screen()
+wn.listen()
+wn.bgcolor('black')
+
 
 # pong ball and walls
 pong = trtl.Turtle()
@@ -6,6 +10,7 @@ pong.up()
 pong.shape('circle')
 pong.color('white')
 pong.shapesize(float(.5))
+
 wall1 = trtl.Turtle()
 wall1.up()
 wall1.shape('square')
@@ -13,6 +18,10 @@ wall1.color('white')
 wall1.shapesize(.5, 10, .5)
 wall1.goto(350, 0)
 wall1.seth(90)
+
+wall1.hideturtle()
+pong.hideturtle()
+
 angle = 0
 
 # move buttons
@@ -22,16 +31,48 @@ def up():
 def dn():
     wall1.bk(10)
 
-#c = 1
-#while (c < 2):
+startgame = False
+startbutton = trtl.Turtle()
+startbutton.color('white')
+startbutton.shapesize(10)
+def stort():
+    global startgame
+    startgame = True
+    wn.startscreen()
+    
+def startscreen():
+    global wn
+    if startgame == False:
+        wn.mainloop()
+        startbutton.onclick(stort)
+        wn.listen()
+        
+    else:
+        startbutton.hideturtle
+        wall1.showturtle()
+        pong.showturtle()
+        wn.onkeypress(up,"Up")
+        wn.onkeypress(dn, "Down")
+        wn.listen()
+        wn.mainloop()
+        
+
+
+
+#while (True):
 #    if (abs(pong.xcor() - wall1.xcor()) < 20):
 #        if (abs(pong.ycor() - wall1.ycor()) < 20):
-#            pong.seth(0 + 180)
+#            pong.seth(angle + 180)
 
 
-wn = trtl.Screen()
-wn.onkeypress(up,"Up")
-wn.onkeypress(dn, "Down")
-wn.listen()
-wn.bgcolor('black')
-wn.mainloop()
+if startgame == False:
+    wn.mainloop()
+    wn.startscreen()
+else:
+    startbutton.hideturtle
+    wall1.showturtle()
+    pong.showturtle()
+    wn.onkeypress(up,"Up")
+    wn.onkeypress(dn, "Down")
+    wn.listen()
+    wn.mainloop()
