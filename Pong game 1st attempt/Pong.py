b50f22af074e3ef8ac86056af5d002b091a5d289
nathaniel-tavarez <nathaniel.tavarez@ptaaschool.net>
2022-03-30 09:02:41 -0500

    more bronk

--- Pong game 1st attempt/Pong.py
+++ Pong game 1st attempt/Pong.py
@@ -1,3 +1,4 @@
+from ossaudiodev import SNDCTL_COPR_WCODE
 import turtle as trtl
 import random as Rand
 
@@ -9,7 +10,7 @@ wn.bgpic(background)
 
 # pong ball and walls
 wall_width, wall_height = .5, 5
-border_width, border_height = 40, .5
+border_width, border_height = 45, .5
 
 pong = trtl.Turtle()
 pong.up()
@@ -65,9 +66,24 @@ bttm_border.shapesize(border_width, border_height)
 bttm_border.goto(0, 300)
 bttm_border.seth(90)
 
+scorecount1 = trtl.Turtle()
+scorecount1.up()
+scorecount1.goto(50, 250)
+scorecount1.pd()
+scorecount1.ht()
+
+scorecount2 = trtl.Turtle()
+scorecount2.up()
+scorecount2.goto(-150, 250)
+scorecount2.pd()
+scorecount2.ht()
+
 curser_size = 20
 angle = 0
 poolean = True
+bluescore = 0
+redscore = 0
+font_setup = ("Arial", 20, "normal")
 
 # move buttons
 def up():
@@ -80,11 +96,16 @@ def dn():
     wall1.bk(10)
 
 # fun buttons
+def reset():
+    pong.goto(0, 0)
+    pong.seth(0)
 def bugfix():
     pong.seth(Rand.randint(0, 360))
 def funbutton():
     global angle
     global poolean
+    global bluescore
+    global redscore
     poolean = False
     while poolean == False:
         pong.fd(5)
@@ -93,10 +114,19 @@ def funbutton():
         if paddle_collision(pong, wall2):
             bugfix()
         if hrzntl_brdr_colide(pong, top_border):
-            pong.seth(Rand.randint(-180, 0))
-        if hrzntl_brdr_colide(pong, bttm_border):
             pong.seth(Rand.randint(0, 180))
-            #bugfix()
+        if hrzntl_brdr_colide(pong, bttm_border):
+            pong.seth(Rand.randint(-180, 0))
+        if scorebordercoll(pong, wall3):
+            redscore += 1
+            scorecount1.clear()
+            scorecount1.write(redscore, font=font_setup)
+            reset()
+        if scorebordercoll(pong, wall4):
+            bluescore += 1
+            scorecount2.clear()
+            scorecount2.write(redscore, font=font_setup)
+            reset()
 
 # start button
 strtbttn = "StartButton.gif"
@@ -112,6 +142,8 @@ def stort():
 
 def run_pong():
     global angle
+    global bluescore
+    global redscore
     while poolean == True:
         pong.fd(5)
         # detect collisions between pong ball and the walls
@@ -136,11 +168,16 @@ def run_pong():
             pong.bk(5)
             pong.seth(angle - 180)
             print(angle - 180)
-        if paddle_collision(pong, wall3):
-            print("point")
-            pong.goto(0,0)  
-        if paddle_collision(pong, wall4):
-            print("point") 
+        if scorebordercoll(pong, wall3):
+            redscore += 1
+            scorecount1.clear()
+            scorecount1.write(redscore, font=font_setup)
+            reset()
+        if scorebordercoll(pong, wall4):
+            bluescore += 1
+            scorecount2.clear()
+            scorecount2.write(redscore, font=font_setup)
+            reset()
 
 
 def paddle_collision(a, b):
@@ -149,6 +186,9 @@ def paddle_collision(a, b):
 def hrzntl_brdr_colide(a, b):
     return abs(a.xcor() - b.xcor()) < curser_size/2 + border_width * curser_size and abs(a.ycor() - b.ycor()) < curser_size/2 + border_height/2
 
+def scorebordercoll(a, b):
+    return abs(a.xcor() - b.xcor()) < curser_size/2 + wall_width * curser_size and abs(a.ycor() - b.ycor()) < curser_size/2 + 999/2
+
 wn.onkeypress(up,"Up")
 wn.onkeypress(dn,"Down")
 wn.onkeypress(LL,"w")
@@ -156,5 +196,6 @@ wn.onkeypress(Ln,"s")
 wn.onkeypress(bugfix,"slash")
 wn.onkeypress(funbutton,"f")
 wn.onkeypress(stort, "space")
+wn.onkeypress(reset, "y")
 wn.listen()
 wn.mainloop()
